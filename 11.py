1ff2c12f3e74c58ca66b3adee76a613196887975
Flavio Augusto <faugpereira@gmail.com>
2021-08-24 18:09:24 -0300

    updated coding

---
+++ 11.py
@@ -0,0 +1,14 @@
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+n3 = int(input('Digite o terceiro número: '))
+n4 = int(input('Digite o quarto número: '))
+
+
+s1 = n1 + n2
+s2 = n3 + n4
+s3 = n1 + n4
+s4 = n
+
+m1 = n1 * n2
+m2 = n3 * n4
+m3 = n1 * n4
