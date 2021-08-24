1242f4c840224192b4ea48a1c1fe4df69588c9ce
Flavio Augusto <faugpereira@gmail.com>
2021-08-24 12:01:39 -0300

    updated coding

--- 3.py
+++ 3.py
@@ -1,5 +1,3 @@
-real = 1147
-
-dolar = real / 5.78
-
-print('\nUsando a cotação do dólar a R$ 5,78 , temos que R$', real,'(nosso salário mínimo atual) corresponde a U$', dolar)
+r = int(input('\nDigite o valor em reais que quer converter para dolar: '))
+d = r/5.28
+print('\nUsando a cotação do dólar a R$ 5,28 temos que R$',r,'corresponde a U$',d)
