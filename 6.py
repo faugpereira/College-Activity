ce016bf8e564b4cbd9a09e0e1694b43bc6d7c448
Flavio Augusto <faugpereira@gmail.com>
2021-08-24 12:11:37 -0300

    updated coding

--- 6.py
+++ 6.py
@@ -1,5 +1,5 @@
-r = 4
+r = int(input('Digite o raio da esfera: '))
 
 v = (4/3)*3.1416*(r**3)
 
-print('\nO volume de uma esfera de raio ', r, ' é igual a: ', v)
+print('\nO volume de uma esfera de raio',r, 'é igual a:', v)
