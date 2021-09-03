58a004f7f184817c6314aebc3a9157e5ec157759
Flavio Augusto <faugpereira@gmail.com>
2021-09-03 00:25:23 -0300

    updated coding

---
+++ 05.py
@@ -0,0 +1,29 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+n3 = int(input('Digite mais um número: '))
+
+maior = -100000
+if (maior < n1):
+    maior = n1
+if (maior < n2):
+    maior = n2
+if (maior < n3):
+    maior = n3
+
+menor = 1000000
+if (menor > n1):
+    menor = n1
+if (menor > n2) :
+    menor = n2
+if (menor > n3):
+    menor = n3
+
+meio = 0
+if (n1 != maior and n1 != menor ):
+    meio = n1
+elif (n2 != maior and n2 != menor):
+    meio = n2
+else:
+    meio = n3
+
+print(menor, meio, maior)
