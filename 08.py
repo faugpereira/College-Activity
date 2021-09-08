208e358337bd5af4926e202274d90f96329cfdab
Flavio Augusto <faugpereira@gmail.com>
2021-09-07 22:37:06 -0300

    updated coding

--- 08.py
+++ 08.py
@@ -1,13 +1,15 @@
 n = int(input('Digite um número: '))
 
 a = 10
-b = 15
+b = 25
 c = 20
-d = 25
+d = 30
 
 if (n>=a) and (n<=b):
     print('O número {} está dentro do intervalo [{},{}]'.format(n, a, b))
-elif (n>=c) and (n<=d):
+if (n>=c) and (n<=d):
     print('O número {} está dentro do intervalo [{},{}]'.format(n, c, d))
-else:
+if (((n>=a) and (n<=b)) and ((n>=c) and (n<=d))):
+    print('O número {} está dentro dos intervalos [{},{}] e [{},{}]'.format(n, a, b, c, d))
+if (((n<a) and (n>b)) and ((n<c) and (n>d))):
     print('O número {} está fora dos intervalos [{},{}] e [{},{}]'.format(n, a, b, c, d))
