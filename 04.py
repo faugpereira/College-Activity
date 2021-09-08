48dc198e06c100603e0d3dc7d03a4938b3c36800
Flavio Augusto <faugpereira@gmail.com>
2021-09-08 19:06:03 -0300

    updated coding

--- 04.py
+++ 04.py
@@ -1,6 +1,6 @@
 s = int(input('Insira seu salário para descobrir quanto receberá após o reajuste: '))
 
-if (s < 1000) & (s > 500):
+if (s <= 1000) and (s >= 500):
     s += s * 15/100
     print('Seu salário receberá reajuste de 15% e será R$ {}'.format(s))
 elif s > 1000:
