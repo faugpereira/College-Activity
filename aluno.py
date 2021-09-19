875a621e62c47eb3bea715da1b6c72231997a337
Flavio Augusto <faugpereira@gmail.com>
2021-09-19 20:50:24 -0300

    updated coding

--- aluno.py
+++ aluno.py
@@ -4,7 +4,9 @@ class Aluno:
         self.curso = curso
         self.p1 = p1
         self.p2 = p2
-        self.m = (p1+p2)/2
+
+    def media(self):
+        return(self.p1+self.p2)/2
 
     def lista(self):
-        print('{} do curso {} tem nota P1 = {} e P2 = {} cuja média é {}'.format(self.nome, self.curso, self.p1, self.p2, self.m))
+        print('{} do curso {} tem nota P1 = {} e P2 = {} cuja média é {}'.format(self.nome, self.curso, self.p1, self.p2, self.media()))
